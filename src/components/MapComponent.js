3bcb4a690902a0a6e7d5405cddb8e2c809ad3786
rafiul-islam <rafiulrony.cse001@gmail.com>
2023-12-12 14:37:23 +0600

    modify MapComponent

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Head from 'next/head';
 import L from 'leaflet';
+import Script from 'next/script';
 
 const MapComponent = () => {
   useEffect(() => {
@@ -26,13 +27,15 @@ const MapComponent = () => {
           integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
           crossOrigin=""
         />
-        <script
+        
+      </Head>
+      <div id="map" style={{ height: '400px', width: '100%' }}></div>
+
+      <Script
           src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
           integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
           crossOrigin=""
-        ></script>
-      </Head>
-      <div id="map" style={{ height: '400px', width: '100%' }}></div>
+        ></Script>
     </>
   );
 };
